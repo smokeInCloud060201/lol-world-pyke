247a564924069f96a09230a85de46109c2e8fef9
Karson Tang (TECHVIFY.D2) <karson.tang@techvify.com.vn>
2024-08-26 17:12:31 +0700

    Initial project

---
+++ api/src/main/java/vn/com/lol/pyke/PykeApplication.java
@@ -0,0 +1,13 @@
+package vn.com.lol.pyke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PykeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PykeApplication.class, args);
+    }
+
+}
---
+++ api/src/test/java/vn/com/lol/pyke/PykeApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.com.lol.pyke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PykeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
